e08b3e27a230cc4a5b91da3309666f9e8428248e
yyzhou94 <yyzhou@njnet.edu.cn>
2021-12-10 21:45:47 +0800

    Update youth_double.js

--- youth_double.js
+++ youth_double.js
@@ -5,7 +5,7 @@ let zq_cookie= $.isNode() ? (process.env.zq_cookie ? process.env.zq_cookie : "")
 let zq_cookieArr = ['zqkey=MDAwMDAwMDAwMJCMpN-w09Wtg5-Bb36eh6CPqHualIejl7&uid=58339453']
 let zq_cookies = ""
 
-if (zq_cookie) {
+if ($.isNode()) {
     if (zq_cookie.indexOf("@") == -1 && zq_cookie.indexOf("@") == -1) {
         zq_cookieArr.push(zq_cookie)
     } else if (zq_cookie.indexOf("@") > -1) {
@@ -13,17 +13,6 @@ if (zq_cookie) {
     } else if (process.env.zq_cookie && process.env.zq_cookie.indexOf('@') > -1) {
         zq_cookieArr = process.env.zq_cookie.split('@');
         console.log(`您选择的是用"@"隔开\n`)
-    }
-} else if($.isNode()){
-    var fs = require("fs");
-    zq_cookie = fs.readFileSync("zq_cookie.txt", "utf8");
-    if (zq_cookie !== `undefined`) {
-        zq_cookies = zq_cookie.split("\n");
-    } else {
-        $.msg($.name, '【提示】进入点击右下角"任务图标"，再跑一次脚本', '不知道说啥好', {
-            "open-url": "给您劈个叉吧"
-        });
-        $.done()
     }
 }
 Object.keys(zq_cookies).forEach((item) => {
